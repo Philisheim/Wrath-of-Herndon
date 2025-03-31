9f0bfe6b809bd0ae1af5c121d3165c3e28a98994
Philisheim <peddie.sermersheim@gmail.com>
2025-03-31 10:42:04 -0500

    Main Menu without difficulty

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace WrathOfHerndon
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public int difficulty = 1;
+
+        // Change difficulty based off of what button is pressed
+        // Difficulty is a number with 1 being low and 3 being high
+        public void Difficulty()
+        {
+            
+        }
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(difficulty);
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
