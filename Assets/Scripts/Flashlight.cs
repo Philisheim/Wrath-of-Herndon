1cfa407263e05618bfdfb369bb8044e80daac477
Kirthika-star <Kirthi.duggisetty@gmail.com>
2025-03-31 10:44:39 -0500

    stuff

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    [SerializeField] GameObject FlashlightLight;
+    private bool FlashlightActive = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FlashlightLight.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (FlashlightActive == false)
+            {
+                FlashlightLight.gameObject.SetActive(true);
+                FlashlightActive = true;
+            }
+            else
+            {
+                FlashlightLight.gameObject.SetActive(false);
+                FlashlightActive = false;
+            }
+        }
+    }
+}
