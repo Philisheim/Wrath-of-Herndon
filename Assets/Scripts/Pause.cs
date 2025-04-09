c1496ce9702e6de9ca50d236122fd89a5155bfbb
DaneDunbar <daneldunbar@gmail.com>
2025-04-09 09:13:41 -0500

    Added fully functional blur effect to the pause menu
    The pause menu can now blur the background of the scene while active. It really was not that hard to add guys, it took 15 minutes tops.

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.SceneManagement;
+
 
 namespace WrathOfHerndon
 {
@@ -11,6 +13,8 @@ namespace WrathOfHerndon
         public GameObject menu;
         public bool isPaused;
         public int typeOfPause = 0;
+        private PostProcessVolume ppVol;
+
         // Start is called before the first frame update
         void Start()
         {
@@ -19,6 +23,7 @@ namespace WrathOfHerndon
 
         private void Update()
         {
+            ppVol = Camera.main.gameObject.GetComponent<PostProcessVolume>();
             if (Input.GetKeyDown(KeyCode.Escape))
             {
                 if (!isPaused)
@@ -33,6 +38,7 @@ namespace WrathOfHerndon
             menu.SetActive(true);
             Time.timeScale = 0f;
             isPaused = true;
+            ppVol.enabled = true;
         }
 
         public void Resume()
@@ -40,10 +46,12 @@ namespace WrathOfHerndon
             menu.SetActive(false);
             Time.timeScale = 1f;
             isPaused = false;
+            ppVol.enabled = false;
         }
 
         public void Exit()
         {
+            ppVol.enabled = false;
             SceneManager.LoadScene(0);
             Time.timeScale = 1f;
         }
